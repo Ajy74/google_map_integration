dabd322f4de1e087ac373e2d89708b59405842c7
Ajay Mourya <mouryaajay7463@gmai.com>
2024-06-14 13:37:46 +0530

    fetching current location

--- lib/views/location_mover_screen.dart
+++ lib/views/location_mover_screen.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class LocationMoverScreen extends StatefulWidget {
@@ -74,6 +76,46 @@ class _LocationMoverScreenState extends State<LocationMoverScreen> {
 
   Future<void> _moveToLocation() async {
     final GoogleMapController controller = await _controller.future;
-    await controller.animateCamera(CameraUpdate.newCameraPosition(moveToLocation));
+    // await controller.animateCamera(CameraUpdate.newCameraPosition(moveToLocation));
+
+    _getCurrentLocation().then((value) async{
+      // if (kDebugMode) {
+      //   print("my current location >>\n$value");
+      // }
+
+      _marker.add(
+        Marker(
+          markerId: const MarkerId('3'),
+          position: LatLng(value.latitude,value.longitude),
+          infoWindow:  const InfoWindow(
+            title: "current location",
+          )
+        )
+      );
+
+      CameraPosition moveToCurrentLocation = CameraPosition(
+        bearing: 192.8334901395799,
+        target: LatLng(value.latitude,value.longitude),
+        tilt: 50.440717697143555,
+        zoom: 19.151926040649414
+      );
+
+      await controller.animateCamera(CameraUpdate.newCameraPosition(moveToCurrentLocation));
+
+      setState(() { });
+
+    });
+  }
+
+  Future<Position> _getCurrentLocation() async {
+    await Geolocator.requestPermission().then((value){
+
+    }).onError((error, stackTrace) {
+      if (kDebugMode) {
+        print("error:-> ${error.toString()}");
+      }
+    });
+
+    return await Geolocator.getCurrentPosition(); 
   }
 }
